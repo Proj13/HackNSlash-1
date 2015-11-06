932f964f7d8020f70cf57fcbd4ea3852b1a26a82
Angel <arsanchez94@embarqmail.com>
2015-11-06 17:18:52 -0500

    ScreenFader
    Added FadingScript, ScreenFader GameObject into Tutorial scene, and
    Splash screen scene. Now when game is opened, the splash screen loads
    with team logo followed by tutorial level, all with faded transitions in
    between.

---
+++ Assets/Scripts/SplashScreenScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashScreenScript : MonoBehaviour {
+	public float timer = 2f;
+	public string levelToLoad = "Main Menu";
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine("DisplayScene");
+	}
+	
+	IEnumerator DisplayScene() {
+		yield return new WaitForSeconds (timer);
+		float fadeTime = GameObject.Find ("ScreenFader").GetComponent<FadingScript> ().BeginFade (1);
+		yield return new WaitForSeconds (fadeTime);
+		Application.LoadLevel (Application.loadedLevel+1);
+	}
+}
